2c6b32576eec4ddd43fd585d64983e12d1e242ea
Evan Flack <evanjflack@gmail.com>
2019-09-21 14:36:02 -0400

    solved for all other non-b2 endogenous variables

--- Projects/Project2/execute.py
+++ Projects/Project2/execute.py
@@ -4,7 +4,7 @@ period-lived overlapping generations model given some parameter values.
 '''
 
 # Put import commands below here
-
+import numpy as np
 
 # Set parameter values
 
@@ -29,7 +29,18 @@ delta = ?
 # Solve for all the other endogenous variables given parameter values
 # and optimal b2
 
+# Solve for all the other endogenous variables given parameter values
+# and optimal b2
+c1 = w*n1 - b2
+c2 = (1 + r)*b2 + w*n2
+C = c1 + c2
 
+r = alpha*A*(L/K)**(1 - alpha) - delta
+w = ((1 - alpha)*A*(K/L))**alpha
 
+K = b2
+L = np.sum(nvec)
+I = K - (1 - delta)*K
+Y = C + I
 
 # Print steady-state equilibrium endogenous variables
